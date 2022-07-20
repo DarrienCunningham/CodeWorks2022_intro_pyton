e70f5ef967893db4c14bb82c6018351409c6da5a
Darrien C <100631754+StarSybil@users.noreply.github.com>
2022-07-20 09:52:40 -0400

    Update user_account.py

--- src/project/user_account.py
+++ src/project/user_account.py
@@ -1,6 +1,6 @@
 users = [{'name:'default user', 'email':'email@email.com', 'password':'1234'}]
 
-def create_user(user_name, email, password1, password2)
+def creat_user(user_name, email, password1, password2)
 
  for user in users:
         if user['email'] != email:
@@ -10,11 +10,24 @@ def create_user(user_name, email, password1, password2)
                 
                 
      def get_user(email):
+       for user in users:
+          if email == user['email']:
+          print(user)
+          break
+          else:
+          print("Error: user not found")
         
         
         
         
-        
-   def get_users():
-    
-    
+   def get_all_users():
+    email_list = []
+          for user in users:
+          email_list.append(user['email'])
+          
+          return email_list
+         
+          
+ creat_user('darrien', 'darrien@adsfwsd.com', '1234', '1234')
+          eamils = get_all_users()
+          print(emails)
