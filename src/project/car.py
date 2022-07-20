5f460d84a9a7fced27f87485b3c0a856ff56704a
DarrienCunningham <100631754+DarrienCunningham@users.noreply.github.com>
2022-07-20 18:15:09 -0400

    Update car.py

--- src/project/car.py
+++ src/project/car.py
@@ -1,12 +1,10 @@
-# define the class
+class Car:
 
-#define the class variables
-
-#define the the init function
-
-#add the methods
-
-
-
-
-#create instance of the class and print them out
+def __init__(self, make, model):
+  self.maake = make
+  self.model = model
+  
+  c = Car('Toyota', 'Camry')
+  
+  def showDescription(self):
+        print("This car is a", self.make, self.model)
