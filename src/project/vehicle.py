68dbec16afcb6532165cc97b00180727c0eb6fa7
DarrienCunningham <100631754+DarrienCunningham@users.noreply.github.com>
2022-07-20 22:03:29 -0400

    done

--- src/project/vehicle.py
+++ src/project/vehicle.py
@@ -1,16 +1,26 @@
 #create the parent class
 class Vehicle:
     def __init__(self,name,color):
-        #your code here
+        self.name = name
+        self.color = color
 
     def max_speed(self):
-        #your code here
-class Car:
+        self.max_speed = 0
+        print(self.max_speed)
+        
+class Car(Vehicle):
     def max_speed(self):
-        #your code here
+        slef.max_speed = 250
+        print(self.max_speed)
 
-class Bus:
+class Bus(Vehicle):
     def max_speed(self):
-        #your code here
+        self.max_speed = 150
+        print(self.max_speed)
+        
 
 #creat instance of the three classes and call the max_speed() function on them
+
+v1 = Vehicle("Veh", "gray").max_speed()
+c1 = Car("car", "black").max_speed()
+b1 = Bus("bus", "white").max_speed()
