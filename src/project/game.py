a87ce7c18fe31a84b4d7842c5c8679ecd33840c5
DarrienCunningham <100631754+DarrienCunningham@users.noreply.github.com>
2022-07-20 21:49:27 -0400

    Update game.py
    done

---
+++ src/project/game.py
@@ -0,0 +1,27 @@
+class Characters:
+ def __init__(self, name, position):
+ self.name = name
+ self.position = position
+ 
+ def move(self):
+ self.position = 0
+ 
+ class Hero(Characters):
+ def move(self):
+ self.position += 1
+ 
+ class Villain(Characters):
+ def move(self):
+ self.position -= 1
+ 
+ c = Characters("char", 0)
+ c.move()
+ print(c.position)
+ 
+ hero = Hero("hero", 0)
+hero.move()
+print(hero.position)
+
+villian = Villian("villian", 0)
+villian.move()
+print(villian.position)
